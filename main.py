b11abb663a4b6bae7e10550117062be23ecfc3ff
Andres Carrillo <andres.carrillo@soytul.com>
2022-02-09 17:27:50 -0500

    Adding try catch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import xlsxwriter
 
 videos = [
     # 'https://www.youtube.com/channel/UCSRDdJckljibYXTbR_7LgOQ/videos' # TEST
-    'https://www.youtube.com/c/Yanbal-CanalOficial/videos',
+    # 'https://www.youtube.com/c/Yanbal-CanalOficial/videos',
     'https://www.youtube.com/user/avoncolombia/videos',
     'https://www.youtube.com/c/MaybellineNewYorkColombia/videos',
     'https://www.youtube.com/c/EsikaBelcorp/videos',
@@ -16,7 +16,7 @@ videos = [
 
 titles = ['Titulo', 'Numero de visitas', 'Duracion',
         'Año de publicación', 'Fecha de publicacion', 'Author', 'URL']
-names = ['Yanbal', 'Avon', 'Maybelline', 'Esika',
+names = ['Avon', 'Maybelline', 'Esika',
          'Natura', 'Masglo', 'Vogue', 'Jolie de vogue']
 counter = 0
 
@@ -35,48 +35,53 @@ for video in videos:
     column = 0
 
     for url in channel_info.url_generator():
-        video_details = YouTube(url)
-        views = video_details.views
-        title = video_details.title
-        author = video_details.author
-        publish_date = video_details.publish_date
-        publish_year = publish_date.year
-        publish_date = publish_date.strftime("%d/%m/%Y")
-        duration = video_details.streaming_data["formats"][0]["approxDurationMs"]
-        duration = int(duration)
+        try:
+            video_details = YouTube(url)
+            views = video_details.views
+            title = video_details.title
+            author = video_details.author
+            publish_date = video_details.publish_date
+            publish_year = publish_date.year
+            publish_date = publish_date.strftime("%d/%m/%Y")
+            duration = video_details.streaming_data["formats"][0]["approxDurationMs"]
+            duration = int(duration)
 
-        seconds = (duration/1000) % 60
-        seconds = int(seconds)
-        minutes = (duration/(1000*60)) % 60
-        minutes = int(minutes)
-        hours = (duration/(1000*60*60)) % 24
+            seconds = (duration/1000) % 60
+            seconds = int(seconds)
+            minutes = (duration/(1000*60)) % 60
+            minutes = int(minutes)
+            hours = (duration/(1000*60*60)) % 24
 
-        duration = "%d:%d:%d" % (hours, minutes, seconds)
+            duration = "%d:%d:%d" % (hours, minutes, seconds)
 
-        print('---------------------------------------------------------')
-        print(f'Titulo: {title}')
-        worksheet.write(row, column, title)
-        column += 1
-        print(f'Numero de visitas: {views}')
-        worksheet.write(row, column, views)
-        column += 1
-        print(f'Duracion: {duration}')
-        worksheet.write(row, column, duration)
-        column += 1
-        print(f'Año de publicacion: {publish_year}')
-        worksheet.write(row, column, publish_year)
-        column += 1
-        print(f'Fecha de publicacion: {publish_date}')
-        worksheet.write(row, column, publish_date)
-        column += 1
-        print(f'Autor: {author}')
-        worksheet.write(row, column, author)
-        column += 1
-        print(f'URL: {url}')
-        worksheet.write(row, column, url)
-        column += 1
-        print('---------------------------------------------------------\n')
-        row += 1
-        column = 0
+            print('---------------------------------------------------------')
+            print(f'Titulo: {title}')
+            worksheet.write(row, column, title)
+            column += 1
+            print(f'Numero de visitas: {views}')
+            worksheet.write(row, column, views)
+            column += 1
+            print(f'Duracion: {duration}')
+            worksheet.write(row, column, duration)
+            column += 1
+            print(f'Año de publicacion: {publish_year}')
+            worksheet.write(row, column, publish_year)
+            column += 1
+            print(f'Fecha de publicacion: {publish_date}')
+            worksheet.write(row, column, publish_date)
+            column += 1
+            print(f'Autor: {author}')
+            worksheet.write(row, column, author)
+            column += 1
+            print(f'URL: {url}')
+            worksheet.write(row, column, url)
+            column += 1
+            print('---------------------------------------------------------\n')
+            row += 1
+            column = 0
+        except:
+            worksheet.write(row, column, url)
+            print(f'FAILED: {url}')
     counter += 1
     workbook.close()
+
