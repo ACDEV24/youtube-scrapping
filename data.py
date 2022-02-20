27e0ff58e317f44ddf76215f8a73d20ee97572e8
Andres Carrillo <andres.carrillo@soytul.com>
2022-02-20 18:35:17 -0500

    Finishing

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 videos = [
     # 'https://www.youtube.com/channel/UCSRDdJckljibYXTbR_7LgOQ/videos' # TEST
     # 'https://www.youtube.com/channel/UCcjjd6Y-zICiunmN_YsdBaQ/videos'  # TEST
-    'https://www.youtube.com/c/Yanbal-CanalOficial/videos',
-    'https://www.youtube.com/user/avoncolombia/videos',
-    'https://www.youtube.com/c/MaybellineNewYorkColombia/videos',
-    'https://www.youtube.com/c/EsikaBelcorp/videos',
-    'https://www.youtube.com/user/NaturaCoOficial/videos',
-    'https://www.youtube.com/c/MASGLOOficial/videos',
-    'https://www.youtube.com/c/VogueCosm%C3%A9ticosColombiaes/videos ',
+    # 'https://www.youtube.com/c/Yanbal-CanalOficial/videos',
+    # 'https://www.youtube.com/user/avoncolombia/videos',
+    # 'https://www.youtube.com/c/MaybellineNewYorkColombia/videos',
+    # 'https://www.youtube.com/c/EsikaBelcorp/videos',
+    # 'https://www.youtube.com/user/NaturaCoOficial/videos',
+    # 'https://www.youtube.com/c/MASGLOOficial/videos',
+    # 'https://www.youtube.com/c/VogueCosm%C3%A9ticosColombiaes/videos ',
     'https://www.youtube.com/user/cosmeticosJolie/videos',
 ]
 
@@ -25,12 +25,12 @@ titles = [
 ]
 
 names = [
-    'Yambal',
-    'Avon',
-    'Maybelline',
-    'Esika',
-    'Natura',
-    'Masglo',
-    'Vogue',
+    # 'Yambal',
+    # 'Avon',
+    # 'Maybelline',
+    # 'Esika',
+    # 'Natura',
+    # 'Masglo',
+    # 'Vogue',
     'Jolie de vogue',
 ]
